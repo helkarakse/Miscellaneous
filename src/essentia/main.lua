f4ccfd01d9e234d08c0d6f7d4fddf2d1b8d4d8bd
helkarakse <nimcuron@gmail.com>
2013-12-09 06:04:07 +0800

    Added the include for functions library and the code to read modem messages to main

--- src/essentia/main.lua
+++ src/essentia/main.lua
@@ -5,4 +5,35 @@
 	Helkarakse, 20131209
 ]]
 
-local peripheralType = "tilejar"
+-- Libraries
+os.loadAPI("functions")
+
+-- Variables
+local jarType = "tilejar"
+local mfrRednetType = "factoryredstonecable"
+local modem
+local modemFrequency = 1
+
+-- Functions
+local modemHandler = function()
+	while true do
+		local _, side, freq, rfreq, message = os.pullEvent('modem_message')
+		functions.debug("Message received from modem: ", message)
+		local array = textutils.unserialize(message)
+		for key, value in pairs(array) do
+			functions.debug(value)
+		end
+	end
+end
+
+local function init()
+	local hasModem, modemDir = functions.locatePeripheral("modem")
+	if (hasModem) then
+		modem = peripheral.wrap(modemDir)
+		modem.open(modemFrequency)
+	end
+	
+	parallel.waitForAll(modemHandler)
+end
+
+init()
