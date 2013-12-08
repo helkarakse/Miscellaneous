f9d8606078082896b98d73eda80fb51fa0237568
helkarakse <nimcuron@gmail.com>
2013-12-09 05:35:38 +0800

    Added the code to detect the jar peripherals adjacent to the computer and store in an array

--- src/essentia/satellite.lua
+++ src/essentia/satellite.lua
@@ -10,19 +10,35 @@ os.loadAPI("functions")
 
 -- Variables
 local peripheralType = "tilejar"
-local modem
 local jars = {}
 
-local refreshLoop = function()
-
+-- Functions
+local function initJars()
+	for _, side in pairs(peripheral.getNames()) do
+		if (peripheral.getType(side) == peripheralType) then
+			local jarPeripheral = peripheral.wrap(side)
+			table.insert(jars, jarPeripheral)
+		end
+	end
+	
+	functions.debug("Jars detected: ", #jars)
 end
 
-local modemHandler = function()
-	
+local refreshLoop = function()
+	while true do
+		sleep(10)
+	end
 end
 
 local function init()
 	local hasModem, modemDir = functions.locatePeripheral("modem")
+	if (hasModem) then
+		modem = peripheral.wrap(modemDir)
+		modem.open(modemFrequency)
+	end
+	
+	initJars()
+--	parallel.waitForAll(refreshLoop)
 end
 
 init()
