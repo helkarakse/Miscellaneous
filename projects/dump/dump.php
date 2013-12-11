85e05050ce9dc9ae56ae0b3b5fe2282c7f324e18
helkarakse <nimcuron@gmail.com>
2013-12-11 10:58:22 +0800

    Added code to differentiate between dimensions when pushing data

--- projects/dump/dump.php
+++ projects/dump/dump.php
@@ -5,17 +5,22 @@
 	 *
 	 */
 
-	$file = "profile.txt";
+	$file = "profile";
+	$fileExt = ".txt";
 	$request = $_GET["req"];
 	
 	if ($request == "push") {
-		$text = urldecode(($_POST["json"]));
-		$handle = fopen($file, "w") or die("Cannot open file: " . $file);
+		$text = urldecode(($_GET["json"]));
+		$dim = $_GET["dim"];
+		$fileName = $file + "-" + $dim + $fileExt;
+		$handle = fopen($fileName, "w") or die("Cannot open file: " . $fileName);
 		fwrite($handle, $text);
 		fclose($handle);
 	} else if ($request == "show") {
+		$dim = $_GET["dim"];
+		$fileName = $file + "-" + $dim + $fileExt;
 		$handle = fopen($file, "r");
-		$data = fread($handle, filesize($file));
+		$data = fread($handle, filesize($fileName));
 		fclose($handle);
 		echo($data);
 	}
