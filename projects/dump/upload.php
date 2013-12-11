3b90fe318b3c5dfaf871582730caf6fa2c3a3c27
helkarakse <nimcuron@gmail.com>
2013-12-11 11:09:54 +0800

    Changed upload.php to push tick files and sort them according to dimension id

--- projects/dump/upload.php
+++ projects/dump/upload.php
@@ -4,22 +4,22 @@
 	 * @author Helkarakse <nimcuron@gmail.com>
 	 *
 	 */
-
-	$file = "profile";
+	// error_reporting(E_ALL);
+	$file = "tick";
 	$fileExt = ".txt";
 	$request = $_GET["req"];
 	
 	if ($request == "push") {
 		$text = urldecode(($_GET["json"]));
 		$dim = $_GET["dim"];
-		$fileName = $file + "-" + $dim + $fileExt;
+		$fileName = $file . "-" . $dim . $fileExt;
 		$handle = fopen($fileName, "w") or die("Cannot open file: " . $fileName);
 		fwrite($handle, $text);
 		fclose($handle);
 	} else if ($request == "show") {
 		$dim = $_GET["dim"];
-		$fileName = $file + "-" + $dim + $fileExt;
-		$handle = fopen($file, "r");
+		$fileName = $file . "-" . $dim . $fileExt;
+		$handle = fopen($fileName, "r");
 		$data = fread($handle, filesize($fileName));
 		fclose($handle);
 		echo($data);
