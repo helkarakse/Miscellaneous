e882fb8d35ea82decf19de8031345d1d033a6f1a
helkarakse <nimcuron@gmail.com>
2013-12-12 10:08:30 +0800

    Updated upload.php and adding timestamp to json array

--- src/upload/upload.php
+++ src/upload/upload.php
@@ -1,27 +1,47 @@
 <?php
-	/*
-	 * PHP TickProfile DataDump Handler
-	 * @author Helkarakse <nimcuron@gmail.com>
-	 *
-	 */
-	// error_reporting(E_ALL);
-	$file = "tick";
-	$fileExt = ".txt";
-	$request = $_GET["req"];
+/*
+ * PHP TickProfile DataDump Handler
+ * @author Helkarakse <nimcuron@gmail.com>
+ *
+ */
+
+// error_reporting(E_ALL);
+header("Content-type: application/json");
+
+$file = "tick";
+$fileExt = ".txt";
+$request = $_GET["req"];
+
+if ($request == "push")
+{
+	$text = urldecode(($_POST["json"]));
 	
-	if ($request == "push") {
-		$text = urldecode(($_POST["json"]));
-		$dim = $_GET["dim"];
-		$fileName = $file . "-" . $dim . $fileExt;
-		$handle = fopen($fileName, "w") or die("Cannot open file: " . $fileName);
-		fwrite($handle, $text);
-		fclose($handle);
-	} else if ($request == "show") {
-		$dim = $_GET["dim"];
-		$fileName = $file . "-" . $dim . $fileExt;
-		$handle = fopen($fileName, "r");
-		$data = fread($handle, filesize($fileName));
-		fclose($handle);
-		echo($data);
+	$array = json_decode($text);
+	$array["last_update"] = date("r", time());
+	$text = json_encode($array);
+	
+	$dim = $_GET["dim"];
+	$fileName = $file . "-" . $dim . $fileExt;
+	$handle = fopen($fileName, "w") or die("Cannot open file: " . $fileName);
+	fwrite($handle, $text);
+	fclose($handle);
+}
+else if ($request == "show")
+{
+	$dim = $_GET["dim"];
+	$fileName = $file . "-" . $dim . $fileExt;
+	
+	$handle = fopen($fileName, "r");
+	$data = fread($handle, filesize($fileName));
+	fclose($handle);
+	
+	if ($_GET["output"] == "json")
+	{
+		print_r($data);
 	}
+	else
+	{
+		echo ($data);
+	}
+}
 ?>
