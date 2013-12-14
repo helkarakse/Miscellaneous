17fbf1cdbe357ea80b84e67ebe1b0a1b0fc19612
helkarakse <nimcuron@gmail.com>
2013-12-15 03:44:26 +0800

    Moved code into methods

--- src/misc/nugget.lua
+++ src/misc/nugget.lua
@@ -1,5 +1,18 @@
 local chest = peripheral.wrap("front")
 local craft_slots = {2, 3, 5, 6, 7, 9, 10, 11}
+
+local function dumpExtras()
+	turtle.select(1)
+	turtle.drop()
+end
+
+local function dumpIngots()
+	turtle.select(2)
+	turtle.turnRight()
+	turtle.drop()
+	turtle.turnLeft()
+end
+
 while true do
 	print("Clearing inventory...")
     for i= 1, 16 do
@@ -15,20 +28,16 @@ while true do
 		if (info ~= nil and (string.find(info["name"], "Nugget")) and info["qty"] > 9) then
 			turtle.select(1)
 			print("Getting items...")
-			chest.pullIntoSlot("east", slotNumber, info["qty"], 1)
+			turtle.suck()
 			local no = turtle.getItemCount(1)
 			for j, k in ipairs(craft_slots) do
 			    turtle.transferTo(k, no/9)
 			end
 			turtle.craft()
 			-- Drop the ingots into the chest
-			turtle.select(2)
-			turtle.turnRight()
-			turtle.drop()
-			turtle.turnLeft()
-			-- Drop the leftover nuggets back into the chest
-		    turtle.select(1)
-		    turtle.drop()
+			dumpIngots()
+			-- Drop the leftover nuggets into another chest
+		    dumpExtras()
 		end
 	    chest.condenseItems()
     end
