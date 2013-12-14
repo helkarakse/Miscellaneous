7099a2f51a54c1e6a19ac2baee599777891469c3
helkarakse <nimcuron@gmail.com>
2013-12-15 03:36:16 +0800

    Debugging

--- src/misc/nugget.lua
+++ src/misc/nugget.lua
@@ -1,20 +1,21 @@
 local chest = peripheral.wrap("front")
 local craft_slots = {2, 3, 5, 6, 7, 9, 10, 11}
 while true do
-    for i=1, 16 do
+	print("Clearing inventory...")
+    for i= 1, 16 do
         if turtle.getItemCount(i)>0 then
             turtle.select(i)
             turtle.drop()
         end
     end
     
-    for i = 1, chest.getInventorySize() do
-		local info = chest.getStackInSlot(i)
-		print("Slot number is now: " .. i)
+    for slotNumber = 1, chest.getInventorySize() do
+		local info = chest.getStackInSlot(slotNumber)
+		print("Slot number is now: " .. slotNumber)
 		if (info ~= nil and (string.find(info["name"], "Nugget")) and info["qty"] > 9) then
 			turtle.select(1)
-			print("push item into slot")
-			turtle.suck()
+			print("Getting items...")
+			chest.pullIntoSlot("east", slotNumber, info["qty"], 1)
 			local no = turtle.getItemCount(1)
 			for j, k in ipairs(craft_slots) do
 			    turtle.transferTo(k, no/9)
