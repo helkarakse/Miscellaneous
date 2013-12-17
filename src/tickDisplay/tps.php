d98b04a569fc1266c50fbfd7a36b1cd95c0afcc7
helkarakse <nimcuron@gmail.com>
2013-12-18 06:54:33 +0800

    Completed the update for tps.php to display last updated date

--- src/tickDisplay/tps.php
+++ src/tickDisplay/tps.php
@@ -2,18 +2,34 @@
 function printTps($array) {
 	foreach ($array as $key => $value) {
 		$tps = round((float)$value, 2);
-		print($tps);
+		print($tps . "<br />");
 	}
 }
 
-$dimension = $_GET["dim"];
-$filename = "tick-" . $dimension . ".txt";
+function printUpdated($array) {
+	foreach ($array as $key => $value) {
+		print($value . "<br />");
+	}
+}
 
-$handle = fopen($filename, "r");
-$json = fread($handle, filesize($filename));
-fclose($handle);
+$dimension = isset($_GET["dim"]) ? $_GET["dim"] : "";
+$date = isset($_GET["date"]) ? $_GET["date"] : false;
 
-$array = ($json != "") ? json_decode($json) : array();
+if ($dimension != "") {
+	$filename = "tick-" . $dimension . ".txt";
 
-(count($array) > 0) ? printTps($array[0]) : print("Unknown");
+	$handle = fopen($filename, "r");
+	$json = fread($handle, filesize($filename));
+	fclose($handle);
+
+	$array = ($json != "") ? json_decode($json) : array();
+
+	(count($array) > 0) ? printTps($array[0]) : print("Unknown" . "<br />");
+
+	if ($date == true && count($array) > 0) {
+		printUpdated($array[5]);
+	}
+} else {
+	echo "Missing variable, dim (dimension).";
+}
 ?>
