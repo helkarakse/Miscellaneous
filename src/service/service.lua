4e772f3567544fd13adb090f51c8e8c5eb1c7179
helkarakse <nimcuron@gmail.com>
2013-12-25 07:31:21 +0800

    Added the code to wrap peripherals

--- src/service/service.lua
+++ src/service/service.lua
@@ -6,3 +6,38 @@ Helkarakse 20131225
 
 ]]
 
+-- Libraries
+os.loadAPI("functions")
+
+-- References
+local functions = functions
+local peripheral = peripheral
+
+-- Variables
+local map, command
+
+-- Functions
+
+-- Handlers
+
+-- Events
+
+-- Loops
+
+-- Main
+local function main()
+	local hasMap, mapDir = functions.locatePeripheral("adventure map interface")
+	if (hasMap) then
+		map = peripheral.wrap(mapDir)
+		functions.debug("Map peripheral detected and wrapped.")
+	else
+		functions.debug("No map peripheral detected. This is required.")
+		return
+	end
+
+	-- Command blocks have no type for peripheral, will have to hard code direction
+	command = peripheral.wrap("left")
+	functions.debug("Command block detected and wrapped.")
+end
+
+main()
