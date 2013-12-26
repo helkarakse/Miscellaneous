88c4e2d25fb6f978c4863a79ed7a0374c68d7b81
helkarakse <nimcuron@gmail.com>
2013-12-26 23:10:15 +0800

    Updated upload.lua to also query for a list of player names

--- src/upload/upload.lua
+++ src/upload/upload.lua
@@ -6,16 +6,22 @@
 	
 ]]
 
+-- Libraries
 os.loadAPI("functions")
+os.loadAPI("json")
 
+-- Variables
 local fileName = "profile.txt"
 local dimension = string.sub(os.getComputerLabel(), 1, 1)
-local outputText
+local server = string.sub(os.getComputerLabel(), 2)
+local outputText, map
 local currentFileSize = 0
 local uploadDelay = 30
--- local urlPush = "http://dev.otegamers.com/helkarakse/tps/upload.php?req=put&dim=" .. dimension
-local urlPush = "http://dev.otegamers.com/framework/index.php/upload/put/server/" .. dimension
 
+-- local urlPush = "http://dev.otegamers.com/helkarakse/tps/upload.php?req=put&dim=" .. dimension
+local urlPush = "http://dev.otegamers.com/framework/index.php/upload/put/" .. server .. "/" .. dimension
+
+-- Functions
 local uploadLoop = function()
 	while true do
 		-- only push the document if it changes
@@ -30,8 +36,11 @@ local uploadLoop = function()
 			
 			-- update the file size to the new one
 			currentFileSize = fs.getSize(fileName)
+
+			-- get the current array of players ingame
+			local playerArray = map.getPlayerUsernames()
 			
-			local response = http.post(urlPush, "json=" .. textutils.urlEncode(outputText))
+			local response = http.post(urlPush, "json=" .. textutils.urlEncode(outputText) .. "&players=" .. textutils.urlEncode(json.encode(playerArray)))
 			if (response) then
 				local responseText = response.readAll()
 				functions.debug(responseText)
@@ -45,6 +54,15 @@ local uploadLoop = function()
 end
 
 local function init()
+	local hasMap, mapDir = functions.locatePeripheral("adventure map interface")
+	if (hasMap) then
+		map = peripheral.wrap(mapDir)
+		functions.debug("Map peripheral detected and wrapped.")
+	else
+		functions.debug("No map peripheral detected. This is required.")
+		return
+	end
+
 	if (fs.exists(fileName)) then
 		parallel.waitForAll(uploadLoop)
 	end
