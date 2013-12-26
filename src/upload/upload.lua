3258ff189aace3c867b2afd47b194c404d9a0005
helkarakse <nimcuron@gmail.com>
2013-12-27 01:55:00 +0800

    Modified upload.lua for unleashed

--- src/upload/upload.lua
+++ src/upload/upload.lua
@@ -12,13 +12,11 @@ os.loadAPI("json")
 
 -- Variables
 local fileName = "profile.txt"
-local dimension = string.sub(os.getComputerLabel(), 1, 1)
-local server = string.lower(string.sub(os.getComputerLabel(), 2))
 local outputText, map = "", nil
 local currentFileSize = 0
 local uploadDelay = 30
 
-local urlPush = "http://dev.otegamers.com/helkarakse/index.php?c=upload&m=put&server=" .. server .. "&type=" .. dimension
+local urlPush = "http://dev.otegamers.com/helkarakse/index.php?c=upload&m=put&server=ftb&type=unleashed"
 
 -- Functions
 local uploadLoop = function()
@@ -35,11 +33,8 @@ local uploadLoop = function()
 			
 			-- update the file size to the new one
 			currentFileSize = fs.getSize(fileName)
-
-			-- get the current array of players ingame
-			local playerArray = map.getPlayerUsernames()
 			
-			local response = http.post(urlPush, "json=" .. textutils.urlEncode(outputText) .. "&players=" .. textutils.urlEncode(json.encode(playerArray)))
+			local response = http.post(urlPush, "json=" .. textutils.urlEncode(outputText))
 			if (response) then
 				local responseText = response.readAll()
 				functions.debug(responseText)
@@ -53,15 +48,6 @@ local uploadLoop = function()
 end
 
 local function init()
-	local hasMap, mapDir = functions.locatePeripheral("adventure map interface")
-	if (hasMap) then
-		map = peripheral.wrap(mapDir)
-		functions.debug("Map peripheral detected and wrapped.")
-	else
-		functions.debug("No map peripheral detected.")
-		return;
-	end
-
 	if (fs.exists(fileName)) then
 		parallel.waitForAll(uploadLoop)
 	end
